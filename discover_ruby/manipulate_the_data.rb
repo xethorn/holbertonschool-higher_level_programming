cc1772f06e048148a04efca12deffe80c7636b50
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-19 15:18:16 -0700

    first commit

---
+++ discover_ruby/manipulate_the_data.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+require 'HTTPClient'
+require 'json'
+
+extheaders = {
+  'User-Agent' => 'Holberton_school',
+  'Authorization' => '5b0fbcd1114ea6d6c375c414f1d48d87ef0b71bb'
+}
+
+http = HTTPClient.new
+response =  http.get_content ('https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc')
+
+parsed = JSON.parse(response)
+
+parsed["items"].map{|e| puts e["full_name"]}.join
--- discover_ruby/write_them_to_disc.rb
+++ discover_ruby/write_them_to_disc.rb
@@ -12,4 +12,5 @@ api = http.get_content ('https://api.github.com/search/repositories?q=language:r
 
 File.write('/tmp/27', api)
 
+
 puts "The File was saved!"
