1008d1cec373f92f0e3ed413992eeb134b52854a
stvngrcia <stevengarciaacuna@gmail.com>
2016-03-19 12:38:20 -0700

    first commit

---
+++ discover_ruby/write_them_to_disc.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+require 'HTTPClient'
+
+extheaders = {
+  'User-Agent' => 'Holberton_school',
+  'Authorization' => '5b0fbcd1114ea6d6c375c414f1d48d87ef0b71bb'
+}
+
+http = HTTPClient.new
+
+api = http.get_content ('https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc')
+
+File.write('/tmp/27', api)
+
+puts "The File was saved!"
