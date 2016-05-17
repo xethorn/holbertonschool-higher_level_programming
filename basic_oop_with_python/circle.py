37a6b243b665e3434cad6d9da2f32ecd732a758e
stvngrcia <stevengarciaacuna@gmail.com>
2016-05-17 14:04:27 -0700

    created public method area

--- basic_oop_with_python/circle.py
+++ basic_oop_with_python/circle.py
@@ -1,5 +1,37 @@
+import math
+'''Class'''
 class Circle():
-    _radious
-    _center
-    _color
-    name
+
+    ''' Constructor '''
+    def __init__(self,radius):
+        # private attributes
+        self.__radius = radius
+        self.__center = [0,0]
+        self.__color = " "
+        # public attributes
+        self.name = " "
+
+    ''' Destructor '''
+    def __del__(self):
+        pass
+
+    ''' Getter '''
+    def get_color(self):
+        return self._color
+
+    ''' Setter '''
+    def set_color(self, color):
+        self._color = color
+
+    ''' Getter '''
+    def get_center(self):
+        return self._center
+
+    ''' Setter '''
+    def set_center(self, center):
+        self._center = center
+
+    ''' Public Method '''
+    def area(self):
+        x = self.__radius**2 * 3.14
+        return x
--- basic_oop_with_python/h_fibonacci.py
+++ basic_oop_with_python/h_fibonacci.py
@@ -12,3 +12,10 @@ def fibonacci(number):
         a = b
         b = s
     return s
+
+''' def fibonacci(number):
+        if number == 0:
+            return 0
+        if number == 1:
+            return 1
+        return fibonacci(number-1) + fibonacci(number-2)'''
