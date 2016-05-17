fb0b3ac44922d9c4ad498877a004633cb88fd3ad
stvngrcia <stevengarciaacuna@gmail.com>
2016-05-17 11:52:17 -0700

    created a python script that prints the alphabet in lower case

---
+++ basic_oop_with_python/0-alphabet.py
@@ -0,0 +1,4 @@
+''' prints the alphabet in lower case letters '''
+import string
+
+print string.ascii_lowercase
---
+++ basic_oop_with_python/h_odd.py
@@ -0,0 +1,6 @@
+'''This function returns 0 is the number passed is even and 1 if is odd '''
+def odd (number):
+    if number%2 == 0:
+        return 0
+    else:
+        return 1
